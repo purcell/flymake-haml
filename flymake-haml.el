0941a92df545d837cf726685aeb5b94aadd26de8
Steve Purcell <steve@sanityinc.com>
2011-11-07 11:12:25 +0000

    Create temp files in system temp dir

--- flymake-haml.el
+++ flymake-haml.el
@@ -14,11 +14,20 @@
 ;;; Code:
 
 (defvar flymake-haml-err-line-patterns '(("^Syntax error on line \\([0-9]+\\): \\(.*\\)$" nil 1 nil 2)))
+
+(defun flymake-haml--create-temp-in-system-tempdir (file-name prefix)
+  "Return a temporary file name into which flymake can save buffer contents.
+
+This is tidier than `flymake-create-temp-inplace', and therefore
+preferable when the checking doesn't depend on the file's exact
+location."
+  (make-temp-file (or prefix "flymake-haml") nil ".haml"))
+
 ;; Invoke utilities with '-c' to get syntax checking
 (defun flymake-haml-init ()
   "Construct a command that flymake can use to check haml source."
   (list "haml" (list "-c" (flymake-init-create-temp-buffer-copy
-                           'flymake-create-temp-inplace))))
+                           'flymake-haml--create-temp-in-system-tempdir))))
 
 ;;;###autoload
 (defun flymake-haml-load ()
